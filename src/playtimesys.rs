47c633ad53ca2bfcee453dc630c866e110afcaa2
Katzchan-code <tylerbuchman21@gmail.com>
2024-07-01 11:16:16 -0400

    added code to make everything disappear on button press

--- src/playtimesys.rs
+++ src/playtimesys.rs
@@ -2,8 +2,8 @@ use bevy::prelude::*;
 use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
 
 
-
-use crate::foodsys::HungerTime;
+use crate::character::Rat;
+use crate::foodsys::{HungerSprite, HungerTime};
 use crate::foodsys::HungerBar;
 use crate::character_movement::StandingTime;
 
@@ -45,25 +45,26 @@ fn playtime_window(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut foodsys_timer: Query<&mut HungerTime>,
     mut movement_timer: Query<&mut StandingTime>,
-    mut hunger_bar: Query<(Entity/*Querying for hungerbar activated state causes crash on startup
-                                    but only when queryed in this file , &mut Activated*/), With<HungerBar>>,
 
-    mut playtime: Query<(Entity, &mut Activated), With <PlaytimeOverlay>>,
+    mut rat_sprite: Query<(Entity, &mut Activated), (With<Rat>, Without<PlaytimeOverlay>, Without<HungerSprite>, Without<HungerBar>)>,
+
+    mut hunger_bar: Query<(Entity, &mut Activated), (With<HungerBar>, Without<PlaytimeOverlay>, Without<HungerSprite>, Without<Rat>)>,
+    mut hunger_sprite: Query<(Entity, &mut Activated), (With<HungerSprite>, Without<HungerBar>, Without<PlaytimeOverlay>, Without<Rat>)>,
+
+    mut playtime: Query<(Entity, &mut Activated), (With <PlaytimeOverlay>, Without<HungerBar>, Without<HungerSprite>, Without<Rat>)>,
 
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>
+    //mut meshes: ResMut<Assets<Mesh>>,
+    //mut materials: ResMut<Assets<ColorMaterial>>
 ) {
     if keyboard_input.just_pressed(KeyCode::KeyX) {
       //pause or unpause timers depending on if X is pressed to bring up minigame screen      
         for mut hunger_timer in &mut foodsys_timer {
             if hunger_timer.timer.paused() == false {
              hunger_timer.timer.pause();
-             println!("X key was pressed to pause the timers")
             }
             else {
              hunger_timer.timer.unpause();
-             println!("X key was pressed to unpause the timers")
             }
 
         for mut standing_timer in &mut movement_timer {
@@ -74,23 +75,54 @@ fn playtime_window(
                 standing_timer.timer.unpause();
             }
         }   
-    //end of pause/unpause code      cant seem to bundle them together, probably doing something wrong but heigh ho off to work we go
+    
+    //hide everything
+        for (rat, mut active_rat) in &mut rat_sprite {
+            if active_rat.active == true {
+                commands.entity(rat).insert(Visibility::Hidden);
+                active_rat.active = false;
+            }
+            else {
+                commands.entity(rat).insert(Visibility::Visible);
+                active_rat.active = true;
+            }
+        }
 
-    for (bar) in &mut hunger_bar {
-        
-    }
+        for (bar, mut active_bar) in &mut hunger_bar {
+            if active_bar.active == true {
+                commands.entity(bar).insert(Visibility::Hidden);
+                active_bar.active = false;
+            }
+            else {
+                commands.entity(bar).insert(Visibility::Visible);
+                active_bar.active = true;
+            }
+        }
+
+        for (hunger_sprite, mut active_hunger_icon) in &mut hunger_sprite {
+            if active_hunger_icon.active == true {
+                commands.entity(hunger_sprite).insert(Visibility::Hidden);
+                active_hunger_icon.active = false;
+            }
+            else {
+                commands.entity(hunger_sprite).insert(Visibility::Visible);
+                active_hunger_icon.active = true;
+            }
+        }
 
     //overlay minigame screen
-    for (playtime_overlay, mut active_window) in &mut playtime {
-        if active_window.active == false {
-            commands.entity(playtime_overlay).insert(Visibility::Visible);
-            active_window.active = true;
+        for (playtime_overlay, mut active_window) in &mut playtime {
+            if active_window.active == false {
+                commands.entity(playtime_overlay).insert(Visibility::Visible);
+                active_window.active = true;
+                println!("X key was pressed to open playtime menu")
+            }
+            else {
+                commands.entity(playtime_overlay).insert(Visibility::Hidden);
+                active_window.active = false;
+                println!("X key was pressed to close playtime menu")
+            }
         }
-        else {
-            commands.entity(playtime_overlay).insert(Visibility::Hidden);
-            active_window.active = false;
-        }
-    }
     }
 } 
 }
